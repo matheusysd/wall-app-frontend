48bb3ebde07aa64d1c81b0c02ef2663af299895b
Matheus Domingos <matheusyurisd@gmail.com>
2021-09-02 20:08:05 -0300

    create axios instance

---
+++ src/services/api.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:4000",
+});
+
+export const login = async (email, password) => {
+  try {
+    const { data, status } = await api.post("/user/login", { email, password });
+
+    return { data, status };
+  } catch (error) {
+    return {
+      data: error.response.data,
+      status: error.response.status,
+    };
+  }
+};
+
+export const createUser = async (userData) => {
+  try {
+    const { data, status } = await api.post("/user/register", { ...userData });
+
+    return { data, status };
+  } catch (error) {
+    return {
+      data: error.response.data,
+      status: error.response.status,
+    };
+  }
+};
+
+export const getPosts = async () => {
+  try {
+    const { data, status } = await api.get("/post");
+    return { data, status };
+  } catch (error) {
+    console.log(error);
+    return {
+      data: error.response.data,
+      status: error.response.status,
+    };
+  }
+};
+
+export const createPost = async (postData, token) => {
+  try {
+    const { data, status } = await api.post(
+      "/post",
+      { ...postData },
+      { headers: { Authorization: token } }
+    );
+
+    return { data, status };
+  } catch (error) {
+    return {
+      data: error.response.data,
+      status: error.response.status,
+    };
+  }
+};
