9f10ba430192887ef3085a48aba676a3a824faa8
Matheus Domingos <matheusyurisd@gmail.com>
2021-09-02 23:18:53 -0300

    feat: create header

---
+++ src/pages/Home/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+export default function Header({ username, authenticated, setIsCreating }) {
+  const history = useHistory();
+
+  function logout() {
+    localStorage.clear();
+    history.push("/");
+  }
+
+  return (
+    <nav className="navbar navbar-dark bg-primary">
+      <a className="navbar-brand" href="/">
+        Home
+      </a>
+      <h4>{username}</h4>
+
+      <div>
+        {authenticated && (
+          <button
+            className="btn btn-success"
+            onClick={() => setIsCreating(true)}
+          >
+            New Post
+          </button>
+        )}
+        <button className="btn btn-danger" onClick={() => logout()}>
+          Logout
+        </button>
+      </div>
+    </nav>
+  );
+}
