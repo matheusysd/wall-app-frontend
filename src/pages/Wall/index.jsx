fc01334fb6831f7c06cf555df29355cc76b09474
Matheus Domingos <matheusyurisd@gmail.com>
2021-09-05 01:07:37 -0300

    remove eslint exception

--- src/pages/Wall/index.jsx
+++ src/pages/Wall/index.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 import { getPosts } from "../../services/api";
 import { useHistory } from "react-router-dom";
